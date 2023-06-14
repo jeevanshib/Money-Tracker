142391b3bcf6f444bea8321368f4dbe338a5b410
jeevanshib <jeevanshi.jb@gmail.com>
2023-06-14 15:30:15 +0530

    Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className='basic'>
+        <input type='text' placeholder={'+200 new items'}></input>
+        <input type='datetime-local'></input>
+        </div>
+        <div className='description'>
+        <input type='text' placeholder={'description'}></input>
+        </div>
+        <button type='submit'>Add new transaction</button>
+      </form>
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Samsung TV</div>
+            <div className='discription'>It was time for new TV</div>
+            </div>
+            <div className='right'>
+              <div className='price'>-$800.00</div>
+              <div className='date'>2021-01-24 23:12:34</div>
+            </div>
+            </div>
+            <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Samsung TV</div>
+            <div className='discription'>It was time for new TV</div>
+            </div>
+            <div className='right'>
+              <div className='price'>-$800.00</div>
+              <div className='date'>2021-01-24 23:12:34</div>
+            </div>
+            </div>
+            <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Samsung TV</div>
+            <div className='discription'>It was time for new TV</div>
+            </div>
+            <div className='right'>
+              <div className='price'>-$800.00</div>
+              <div className='date'>2021-01-24 23:12:34</div>
+            </div>
+            </div>
+      </div>
+    </main>
   );
 }
 
